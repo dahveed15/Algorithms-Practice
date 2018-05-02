7c56fda21e85ffc5b4319922533507889a729196
David Harris <dahveed15@gmail.com>
2018-05-02 15:01:34 -0700

    single number problem

--- ruby_problems.rb
+++ ruby_problems.rb
@@ -15,3 +15,18 @@ def remove_duplicates(nums)
  nums.length
 
 end
+
+def single_number(nums)
+
+  hash = Hash.new(0)
+
+  nums.each do |num|
+    hash[num] += 1
+  end
+
+  hash.each do |k, v|
+    return k if v == 1
+  end
+
+
+end
